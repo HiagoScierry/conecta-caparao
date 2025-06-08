b22e0949642c9aa5ca46b6a7b00b99c1bf1d3c0d
Hiago Moreira <hiagocarlosmoreira@hotmail.com>
2025-06-08 12:10:58 -0300

    criado pagina route para a pagina de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1>Hello World</h1>
+      <Link href="/entrar">
+        <Button className="bg-tourism-primary text-white hover:bg-tourism-dark transition-colors">
+          Acessar Painel
+        </Button>
+      </Link>
     </div>
   );
 }
