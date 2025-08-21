f89d9a65dd14eb4190b440e62ddf712021fcd455
Gabriel Souza <gabriel_souzasss@outlook.com>
2025-08-21 16:36:19 -0300

    Refinando página de atrativos e adicionando resposividaede mobile

--- src/app/atrativos/[id]/page.tsx
+++ src/app/atrativos/[id]/page.tsx
@@ -4,26 +4,28 @@ import { faker } from "@faker-js/faker";
 import { Informacoes } from "@/components/public/Informacoes";
 import { Badge } from "@/components/ui/badge";
 import { HeroGaleria } from "@/components/public/HeroGaleria";
+import { GaleriaDeImagens } from "@/components/public/GaleriaDeImagens";
 
 export default function PageAtrativo({ params }: { params: { id: string } }) {
   return (
     <LayoutPublic>
-      <HeroGaleria
-        imagemUrls={Array.from({ length: 5 }, () => faker.image.url())}
+      <GaleriaDeImagens
+        imagemUrls={Array.from({ length: 5 }, () => faker.image.url({ width: 800, height: 600 }))}
       />
-      <div className="flex justify-between items-start p-4 w-full h-full bg-tourism-branco">
+      <div className="flex flex-col md:flex-row p-4 lg:px-16 w-full h-full bg-tourism-branco">
         <DescriptionSection
           subtitulo={faker.company.name()}
           descricao={faker.lorem.paragraphs(5)}
-          corSubtitulo={"#000"}
+          corSubtitulo={"text-tourism-azul"}
           corDescricao={"#000"}
+          align="left"
         />
-        <div className="w-1/4 h-full bg-[#0096E1] mt-8">
-          <h1 className="text-white text-xl p-2">Perfil</h1>
+        <div className=" md:1/4 h-full bg-tourism-azul  mt-8">
+          <h1 className="text-white text-xl p-2 border-b-2">Perfil</h1>
           {Array.from({ length: 5 }).map((_, index) => (
             <Badge
               key={index}
-              className="m-1"
+              className="m-2"
               style={{
                 backgroundColor: faker.color.human(),
                 color: "#fff",
--- src/components/public/DescriptionSection.tsx
+++ src/components/public/DescriptionSection.tsx
@@ -6,12 +6,13 @@ interface DescriptionSectionProps {
     descricao?: string;
     corSubtitulo?: string;
     corDescricao?: string;
+    align?: 'center' | 'left';
 }
 
-export function DescriptionSection({ subtitulo, descricao, corSubtitulo, corDescricao }: DescriptionSectionProps) {
+export function DescriptionSection({ subtitulo, descricao, corSubtitulo, corDescricao, align = 'center' }: DescriptionSectionProps) {
     return (
         <section className="w-full bg-tourism-branco py-12">
-            <div className="container mx-auto flex flex-col items-center text-center px-4">
+            <div className={cn("container mx-auto flex flex-col  items-center text-center  px-4", {"md:items-start md:text-start": align==="left"} )}>
                 {subtitulo && <h2 className={cn("text-2xl md:text-4xl font-bold mb-8 max-w-3xl", corSubtitulo)}>{subtitulo}</h2>}
                 {descricao && <p className={cn("md:text-lg lg:text-xl max-w-3xl break-words", corDescricao)}>{descricao}</p>}
             </div>
