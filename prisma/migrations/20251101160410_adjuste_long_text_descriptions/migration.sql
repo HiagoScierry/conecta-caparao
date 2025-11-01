0eef9b6729c10e9db34703ef5ba961c9d5c493eb
Hiago Moreira <hiagocarlosmoreira@hotmail.com>
2025-11-01 13:04:24 -0300

    criado migraçao

---
+++ prisma/migrations/20251101160410_adjuste_long_text_descriptions/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE `AtracaoTuristica` MODIFY `descricao` LONGTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `Evento` MODIFY `descricao` LONGTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `Municipio` MODIFY `descricao` LONGTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `ServicoTuristico` MODIFY `descricao` LONGTEXT NULL;
