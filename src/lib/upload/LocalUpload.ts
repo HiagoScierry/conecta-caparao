0d685135c1b69a6b61525b8119644b01b0655083
Hiago Moreira <hiagocarlosmoreira@hotmail.com>
2025-11-13 12:24:14 -0300

    criado servico para upload no s3

--- src/lib/upload/LocalUpload.ts
+++ src/lib/upload/LocalUpload.ts
@@ -1,6 +1,6 @@
 import { IUpload } from "./IUpload";
-import fs from "fs";
-import path from "path";
+import * as fs from "fs";
+import * as path from "path";
 
 export class LocalUpload implements IUpload {
   private readonly uploadDir: string;
